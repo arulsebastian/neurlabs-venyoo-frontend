0ace9f1050ca98a8fd8fae9d200d5a95a5f8ecb1
Lev Taydakov <lev.taydakov@gmail.com>
2015-07-09 14:32:39 -0700

    Rebuild structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "stylesheets/base";
+
+import Container from "javascripts/container";
+
+React.render(<Container />, document.querySelector("#main"));
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+
+const sassLoaders = [
+  "css-loader",
+  "autoprefixer-loader?browsers=last 2 version",
+  "sass-loader?indentedSyntax=sass&includePaths[]=" + path.resolve(__dirname, "./src"),
+];
+
+const config = {
+  entry: {
+    app: ["./src/index"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"],
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract("style-loader", sassLoaders.join("!")),
+      },
+    ]
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "./build"),
+    publicPath: "/",
+  },
+  plugins: [
+    new ExtractTextPlugin("[name].css"),
+  ],
+  resolve: {
+    extensions: ["", ".js", ".sass"],
+    modulesDirectories: ["src", "node_modules"],
+  },
+};
+
+module.exports = config;
