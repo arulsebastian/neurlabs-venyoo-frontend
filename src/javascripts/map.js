196565a82b4a91a3806e50a8b8e3e2d6ac40e4b5
Lev Taydakov <lev.taydakov@gmail.com>
2015-07-13 10:35:12 -0700

    Add random default markers to the map

--- src/javascripts/map.js
+++ src/javascripts/map.js
@@ -1,18 +1,41 @@
 /* JS dependencies */
 import React from 'react';
-import {GoogleMaps} from 'react-google-maps';
+import {GoogleMaps, Marker} from 'react-google-maps';
 
 /* Static dependencies */
 import mapImage from '../images/map.jpg';
 
 export default class Map extends React.Component {
+  constructor (...args) {
+    super(...args);
+    var markers = generateMarkers(20);
+
+    this.state = {
+      markers: markers
+    };
+
+    function generateMarkers (num) {
+      var markers = [];
+      for (var i = 0; i < num; i++) {
+        markers.push({
+          position: {
+            lat: Math.random() * 180 - 90,
+            lng: Math.random() * 360 - 180
+          },
+          key: "Marker #" + i,
+          animation: 3
+        });
+      }
+      return markers;
+    }
+  }
+
   render () {
     const {props, state} = this;
           // {googleMapsApi, ...otherProps} = props;
 
     return (
       <div className='map_sec'>
-        {/* <img src={mapImage} alt='' /> */}
         <GoogleMaps
           containerProps = {{
             // ...otherProps,
@@ -21,10 +44,23 @@ export default class Map extends React.Component {
               height: '400px'
             }
           }}
-          googleMapsApi = {google.maps}
-          zoom = {3}
-          center = {{ lat: -25.363882, lng: 131.044922 }} />
+          googleMapsApi = { google.maps }
+          zoom = { 1 }
+          center = {{ lat: 25.363882, lng: 131.044922 }}>
+          { state.markers.map(toMarker) }
+        </GoogleMaps>
       </div>
     );
+
+    function toMarker (marker, index) {
+      return (
+        <Marker
+        position={marker.position}
+        key={marker.key}
+        animation={marker.animation} />
+      );
+    }
   }
+
+
 }
