76ee0175927540b8e15b3381ba76d25a93447c33
Lev Taydakov <lev.taydakov@gmail.com>
2015-07-14 09:55:35 -0700

    Integrate bootstrap-slider

--- src/javascripts/timeslider.js
+++ src/javascripts/timeslider.js
@@ -1,15 +1,32 @@
 /* JS dependencies */
 import React from 'react';
+import Slider from 'bootstrap-slider';
 
-/* Stylesheet dependencies */
+/* Static dependencies */
+import 'bootstrap-slider/dist/css/bootstrap-slider.min.css';
 import sliderImage from '../images/start_image.jpg';
 
 export default class TimeSlider extends React.Component {
 	render () {
 		return (
 			<div className='start_detail'>
-				TimeSlider
+				<input 
+					id="ex1"
+					data-slider-id='ex1Slider'
+					type="text"
+					data-slider-min="0"
+					data-slider-max="20"
+					data-slider-step="1"
+					data-slider-value="14" />
 			</div>
 		);
+	}
+
+	componentDidMount () {
+		var slider = new Slider('#ex1', {
+			formatter: function (value) {
+				return 'Current value: ' + value;
+			}
+		});
 	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ const config = {
 				loaders: ["babel"],
 			},
 			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract("style", "css")
+			},
+			{
 				test: /\.scss$/,
 				loader: ExtractTextPlugin.extract("style", "css!sass")
 			},
