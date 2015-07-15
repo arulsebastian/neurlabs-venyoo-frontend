a11a7602e70e8184dbaf7832c5d8fb705fc3a4eb
Lev Taydakov <lev.taydakov@gmail.com>
2015-07-14 22:42:00 -0700

    Replace bootstrap-slider with a canvas

--- src/javascripts/timeslider.js
+++ src/javascripts/timeslider.js
@@ -3,32 +3,40 @@ import React from 'react';
 import Slider from 'bootstrap-slider';
 
 /* Static dependencies */
-import 'bootstrap-slider/dist/css/bootstrap-slider.min.css';
 import sliderImage from '../images/start_image.jpg';
 
 export default class TimeSlider extends React.Component {
 	render () {
 		return (
 			<div className='start_detail'>
-				<input 
-					id="ex1"
-					data-slider-id='ex1Slider'
-					type="text"
-					data-slider-min="0"
-					data-slider-max="20"
-					data-slider-step="1"
-					data-slider-value="14"
-					data-slider-handle="triangle"
-					style={{ width : 350 }} />
+				<img src={sliderImage} />
+				
+				<div className='slider_start_label'>
+					Start
+				</div>
+				<div className='slider_finish_label'>
+					Finish
+				</div>
+				<div className='slider_timescale' ref='sliderTimescale'>
+					<div>
+					</div>
+				</div>
+
 			</div>
 		);
 	}
 
 	componentDidMount () {
-		var slider = new Slider('#ex1', {
-			formatter: function (value) {
-				return 'Current value: ' + value;
-			}
+		var timeScale = React.findDOMNode(this.refs.sliderTimescale);
+
+		timeScale.addEventListener("mousedown", function () {
+			console.log('mouse is down');
+		});
+		timeScale.addEventListener("mouseup", function () {
+			console.log('mouse is up');
+		});
+		timeScale.addEventListener("mousemove", function () {
+			console.log('mouse is moving');
 		});
 	}
 }
