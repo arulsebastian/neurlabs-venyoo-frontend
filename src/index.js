ea69b109e6025c5ff25d1bd58840fd3f982581a8
Lev Taydakov <lev.taydakov@gmail.com>
2015-07-15 17:31:07 -0700

    Compile

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/* JS dependencies */
-import React from "react";
-import Container from "javascripts/container";
-
-/* Static dependencies */
-import "./index.html";
-import "stylesheets/base";
-
-React.render(<Container />, document.querySelector("#main"));
